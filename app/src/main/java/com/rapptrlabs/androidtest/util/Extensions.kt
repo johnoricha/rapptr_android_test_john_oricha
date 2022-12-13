dd5220e1dd37f36b96cbf58d543cb764293cd2c3
johnoricha <johnrichman664@gmail.com>
2022-12-13 15:31:17 +0100

    minor changes

--- app/src/main/java/com/rapptrlabs/androidtest/util/Extensions.kt
+++ app/src/main/java/com/rapptrlabs/androidtest/util/Extensions.kt
@@ -4,14 +4,13 @@ import com.google.android.material.textfield.TextInputLayout
 
 object Extensions {
     fun TextInputLayout.checkValidation(inputValue: Boolean, errorMsg: String) {
-        if (!inputValue) {
-//            Timber.d(errorMsg)
-            this.error = errorMsg
-            this.isErrorEnabled = true
-        } else {
-//            Timber.d("free")
+        if (inputValue) {
             this.error = ""
             this.isErrorEnabled = false
+
+        } else {
+            this.error = errorMsg
+            this.isErrorEnabled = true
         }
     }
 }
